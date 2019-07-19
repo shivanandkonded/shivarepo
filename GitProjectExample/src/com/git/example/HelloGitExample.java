8a8dca587e3bb7e5d6cea771b83f716776861f57
shiva <shivanandkonded@gmail.com>
2019-07-19 13:52:13 +0530

    changes in HelloGitExample

--- GitProjectExample/src/com/git/example/HelloGitExample.java
+++ GitProjectExample/src/com/git/example/HelloGitExample.java
@@ -3,5 +3,9 @@ package com.git.example;
 public class HelloGitExample {
 	public static void main(String[] args) {
 		System.out.println(" Am in the Git Example ");
+		
+		for(int i= 0;i<10;i++) {
+			System.out.println(" Hello World.... !" +i);
+		}
 	}
 }
